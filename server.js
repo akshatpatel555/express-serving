10f2240fc26d6187246d27fcf10a4bb1eb96ded7
akshatpatel555 <akshatpatel555@gmail.com>
2021-11-18 15:22:44 -0800

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,33 @@ app.use(function(req, res, next){
     // res.send()
 })
 
+//routing middleware function
+//will only be triggered if methods match. (html method get, and url /about)
+app.get('/about', function(req, res, next){ 
+    console.log("-- about page requested")
+    res.satus(200)
+    res.send("<html><body><h1>About Page</h1></body></html>")
+})
+app.get('/', function(req, res, next){
+    console.log("-- home page requested")
+    res.status(200).sendFile(_dirname + '/public/index.html')
+})
+
+app.get('/:user/status/:id', function(req, res, next){
+    console.log("tweet page requested")
+    console.log("req.params", req.perams)
+    next()
+})
+
+app.get('*', function(req, res, next){
+    console.log("404!")
+    res.status(404).sendFile(_dirname + '/public/404.html')
+})
+
+// app.post()
+// app.patch()
+// app.put()
+
 
 app.listen(8000, function(){
     console.log("== server is listening to port 8000")
